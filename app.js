ac38bdd128bb4bbcb66bac8a77a990fdadf2a4fe
asterchang96@gmail.com <asterchang96@gmail.com>
2021-08-26 10:29:46 +0800

    feat: install bcrypt/body-parser and add userController and signup/signin page

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
+const bodyParser = require('body-parser')
 const db = require('./models')
 const app = express()
 const port = 3000
@@ -7,6 +8,7 @@ const port = 3000
 // 設定 view engine 使用 handlebars
 app.engine('handlebars', handlebars({ defaultLayout : 'main' }))
 app.set('view engine', 'handlebars')
+app.use(bodyParser.urlencoded({ extended: true }))
 
 
 app.listen(port, () => {
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const restController = require('../controllers/restController.js')
 const adminController = require('../controllers/adminController.js')
+const userController = require('../controllers/userController.js')
 
 module.exports = (app) => {
 
@@ -11,7 +12,9 @@ module.exports = (app) => {
 
   //在 /restaurants 底下則交給 restController.function 處理
   app.get('/restaurants', restController.getRestaurants)
-
+  
+  app.get('/signup', userController.signUpPage)
+  app.post('/signup', userController.signUp)
 
   //後台
   app.get('/admin', (req, res) => {
