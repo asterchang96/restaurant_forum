7521ba8f26979fbd36bf1f0e6ed24c6fbcad73cb
asterchang96@gmail.com <asterchang96@gmail.com>
2021-08-27 10:01:08 +0800

    feat: Add admin restaurant create form and post feature

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -6,6 +6,29 @@ const adminController = {
     return Restaurant.findAll({ raw:true }).then(restaurants =>{
       return res.render('admin/restaurants', { restaurants })
     })
+  },
+
+  creatRestaurant: (req, res) => {
+    return res.render('admin/create')
+  },
+
+  postRestaurant: (req, res) => {
+    const { name, tel, address, opening_hours, description } = req.body
+    if(!name){
+      req.flash('error_messages', '名字不存在！')
+      return res.redirect('back')
+    }
+    return Restaurant.create({ 
+      name: name,
+      tel: tel,
+      address: address,
+      opening_hours: opening_hours,
+      description: description
+    })
+    .then((restaurant) =>{
+      req.flash('success_messages', '餐廳已成功建立！')
+      res.redirect('/admin/restaurants')
+    })
   }
 }
 
