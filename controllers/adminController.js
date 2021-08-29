59368207e3cdbe9b41faa9c020d5010eb501d860
asterchang96@gmail.com <asterchang96@gmail.com>
2021-08-29 09:49:33 +0800

    Install Imgur api and update admin POST and PUT

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,6 +1,8 @@
 const db = require('../models')
 const Restaurant = db.Restaurant
 const fs = require('fs')
+const imgur = require('imgur-node-api')
+const IMGUR_CLIENT_ID = '92ae2a0a67d0121'
 
 const adminController = {
   getRestaurants: (req, res) => {
@@ -22,22 +24,20 @@ const adminController = {
       return res.redirect('back')
     }
     if(file){
-      fs.readFile(file.path, (err, data) => {
-        if (err) console.log('Error: ', err)
-        fs.writeFile(`upload/${file.originalname}`, data, () => {
-          return Restaurant.create({ 
-            name: name,
-            tel: tel,
-            address: address,
-            opening_hours: opening_hours,
-            description: description,
-            image: file ? `/upload/${file.originalname}` : null
-          })
+      imgur.setClientID(IMGUR_CLIENT_ID)
+      imgur.upload(file.path, (err, img) => {
+        return Restaurant.create({ 
+          name: name,
+          tel: tel,
+          address: address,
+          opening_hours: opening_hours,
+          description: description,
+          image: file ? img.data.link : null,
+        })
           .then((restaurant) => {
             req.flash('success_messages', '餐廳已成功建立！')
             res.redirect('/admin/restaurants')
           })
-        })
       })
     }else{
       return Restaurant.create({
@@ -49,7 +49,7 @@ const adminController = {
         image: null
     }).then((restaurant) =>{
       req.flash('success_messages', '餐廳已成功建立！')
-      res.redirect('/admin/restaurants')
+      return res.redirect('/admin/restaurants')
     })
     }
   },
@@ -67,7 +67,6 @@ const adminController = {
   },
 
 
-  //TODO: file(req) 和 一般資料(req.body)存放不同地方?
   putRestaurant: (req, res) => {
     const { name, tel, address, opening_hours, description } = req.body
     const { file } = req
@@ -77,25 +76,23 @@ const adminController = {
       return res.redirect('back')
     }
     if(file){
-      fs.readFile(file.path, (err, data) => {
-        if(err) console.log(err)
-        fs.writeFile(`upload/${file.originalname}`, data, () => {
-          return Restaurant.findByPk(req.params.id)
-            .then((restaurant) => {
-              restaurant.update({
-                name: name,
-                tel:tel,
-                address:address,
-                opening_hours: opening_hours,
-                description: description,
-                image: file ? `/upload/${file.originalname}` : restaurant.image //更新或保留原本值
+      imgur.setClientID(IMGUR_CLIENT_ID);
+      imgur.upload(file.path, (err, img) => {
+        return Restaurant.findByPk(req.params.id)
+          .then((restaurant) => {
+            restaurant.update({
+              name: name,
+              tel:tel,
+              address:address,
+              opening_hours: opening_hours,
+              description: description,
+              image: file ? img.data.link : restaurant.image //更新或保留原本值
               })
               .then((restaurant) => {
                 req.flash('success_messages', "餐廳已成功修改！")
                 res.redirect('/admin/restaurants')
               })
             })          
-        })
       })
     }else{
       return Restaurant.findByPk(req.params.id)
