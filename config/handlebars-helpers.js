ce77ff3fd98ecd224c3aae262c5ff14a4a58648f
asterchang96@gmail.com <asterchang96@gmail.com>
2021-09-03 22:52:08 +0800

    Show related comments on restaurant page and Convert time format using moment

--- config/handlebars-helpers.js
+++ config/handlebars-helpers.js
@@ -1,8 +1,13 @@
+const moment = require('moment')
+
 module.exports = {
   ifCond: function (a, b, options) {
     if (a === b) {
       return options.fn(this)
       }
     return options.inverse(this)
+  },
+  moment: function(a){ //將絕對時間換成相對時間
+    return moment(a).fromNow()
   }
 }
