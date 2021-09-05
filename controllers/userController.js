b78f11b6363aa33dd672ab8918e5f1b4cf401a26
asterchang96@gmail.com <asterchang96@gmail.com>
2021-09-05 22:03:50 +0800

    Add addFollowing and removeFollowing to userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,6 +5,8 @@ const Comment = db.Comment
 const Restaurant = db.Restaurant
 const Favorite = db.Favorite
 const Like = db.Like
+const Followship = db.Followship
+
 const imgur = require('imgur-node-api')
 const IMGUR_CLIENT_ID = process.env.IMGUR_CLIENT_ID
 
@@ -159,6 +161,7 @@ const userController = {
       })
       .catch((error) => {console.log(error)})
   },
+
   getTopUser: (req, res) => {
     return User.findAll({
       include: [
@@ -173,6 +176,29 @@ const userController = {
       users = users.sort((a, b) => b.FollowerCount - a.FollowerCount)
       return res.render('topUser', { users: users })
     })
+  },
+
+  addFollowing: (req, res) => {
+    return Followship.create({
+      followerId: req.user.id,
+      followingId: req.params.userId
+    })
+      .then((followship) => {
+        return res.redirect('back')
+      })
+    },
+
+  removeFollowing: (req, res) => {
+    return Followship.findOne({where: {
+      followerId: req.user.id,
+      followingId: req.params.userId
+    }})
+      .then((followship) => {
+        followship.destroy()
+          .then((followship) => {
+            return res.redirect('back')
+          })
+      })
   }
 
 }
--- routes/index.js
+++ routes/index.js
@@ -66,7 +66,9 @@ module.exports = (app, passport) => {
   app.delete('/favorite/:restaurantId', authenticated, userController.removeFavorite)
   app.post('/like/:restaurantId', authenticated, userController.addLike)
   app.delete('/like/:restaurantId', authenticated, userController.removeLike)  
-
+  app.post('/following/:userId', authenticated, userController.addFollowing)
+  app.delete('/following/:userId', authenticated, userController.removeFollowing)
+  
   app.get('/signup', userController.signUpPage)
   app.post('/signup', userController.signUp)
 
